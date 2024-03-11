061977debaafc889a8934b9011123a111a7a56b7
Yeisonpg <101079820+Yeisonpgh@users.noreply.github.com>
2024-03-11 17:23:25 -0500

    Initial commit

--- script.js
+++ script.js
@@ -13,11 +13,11 @@ const password = document.getElementById('password');
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  if ((username.value === 'Yeison0' && password.value === 'TheGame') || 
-      (username.value === 'Yeison1' && password.value === 'Qwerty') ||
-      (username.value === 'Yeison2' && password.value === 'Heads') ||
-      (username.value === 'Yeison3' && password.value === 'Zetas') ||
-      (username.value === 'Yeison4' && password.value === 'Cartel')) {
+  if ((username.value === 'Yeison1' && password.value === 'TheGame') || 
+      (username.value === 'Yeison2' && password.value === 'Qwerty') ||
+      (username.value === 'Yeison3' && password.value === 'Heads') ||
+      (username.value === 'Yeison4' && password.value === 'Zetas') ||
+      (username.value === 'Yeison5' && password.value === 'Cartel')) {
     if (username.value === 'Yeison0') {
       window.location.href = 'https://streamable.com/xnq0jh';
     } else if (username.value === 'Yeison1') {
