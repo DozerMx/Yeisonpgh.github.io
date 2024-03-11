ffe7a5b775c5b4559833f00c22cbbd56356f9b03
Yeisonpg <101079820+Yeisonpgh@users.noreply.github.com>
2024-03-10 20:24:42 -0500

    Initial commit

--- script.js
+++ script.js
@@ -13,8 +13,13 @@ const password = document.getElementById('password');
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  if (username.value === 'Yeison0' && password.value === 'TheGame') {
-    window.location.href = 'https://streamable.com/xnq0jh';
+  if ((username.value === 'Yeison0' && password.value === 'TheGame') || 
+      (username.value === 'Yeison1' && password.value === 'Qwerty')) {
+    if (username.value === 'Yeison0') {
+      window.location.href = 'https://streamable.com/xnq0jh';
+    } else if (username.value === 'Yeison1') {
+      window.location.href = 'https://streamable.com/586os1';
+    }
   } else {
     showAlert();
   }
